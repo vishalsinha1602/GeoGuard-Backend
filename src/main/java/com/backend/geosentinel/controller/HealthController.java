c4a72148ba4b2a6e5921e22d9595f3971a00a661
Your Name <vishalsinha.inbox@gmail.com>
2026-09-11 16:51:29 +0530

    adding health url

---
+++ src/main/java/com/backend/geosentinel/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.backend.geosentinel.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Application is running");
+    }
+}
